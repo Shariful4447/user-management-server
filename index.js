0b4c03ea9fd7faa8ef16e13295288afcffc35247
Shariful4447 <shariful.islam4447@gmail.com>
2024-01-19 12:43:41 +0100

    add data from client side

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
-
+//middleware
+app.use(cors());
+app.use(express.json());
 const users = [
     {id: 1, name:'sabana'},
     {id: 2, name:'sabana2'},
@@ -14,6 +17,16 @@ app.get('/', (req, res) =>{
 app.get('/users', (req, res) =>{
  res.send(users);
 })
+
+//post for new user###
+app.post('/users', (req, res) =>{
+    console.log('req.body');
+    const newUser = req.body;
+    newUser.id = users.length + 1; 
+    users.push(newUser);
+    res.send(newUser);
+})
+
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`);
 });
---
+++ steps.js
@@ -0,0 +1,4 @@
+/**
+ *1. create a post on the server sider for adding new use
+ *2. client side send request 
+*/
